d0d26d98ad69bec350eeb2b511e950165a10ed0c
Obaid Ur Rehman <obaidjany@gmail.com>
2025-11-30 20:42:05 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./Pages/header";
+import Header from "./Pages/Header";
 import Hero from "./Pages/hero";
 import About from "./Pages/about";
 import Skills from "./Pages/skillssection";
