a61b406355ac5860b654e3a92a7cf6294bc09113
Obaid Ur Rehman <obaidjany@gmail.com>
2025-11-30 20:45:44 +0500

    Update Header.jsx

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from '../Components/logo'
 import NavBar from '../Components/NavBar'
 
-const header = () => {
+const Header = () => {
   return (
     <>
     <header className='flex justify-between items-center px-10 py-2'>
@@ -14,4 +14,4 @@ const header = () => {
   )
 }
 
-export default header
+export default Header
