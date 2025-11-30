3a9ffc0a25dbdc7bf254a93d47f0dbae85c9f73d
Obaid Ur Rehman <obaidjany@gmail.com>
2025-11-30 21:03:12 +0500

    Update Header.jsx

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import LogoPfp from "../Components/Logo";
+// import LogoPfp from "../Components/Logo";
 import NavBar from '../Components/NavBar'
 
 const Header = () => {
   return (
     <>
     <header className='flex justify-between items-center px-10 py-2'>
-      <LogoPfp />
+      {/* <LogoPfp /> */}
       <NavBar/>
     </header>
       
